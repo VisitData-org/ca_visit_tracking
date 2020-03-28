4789b806f0090d2fc5a42d4d755fb46ef499db18
Mark Roth <mark@octagonsoftware.com>
2020-03-28 17:19:27 -0400

    Root redirects to /index.html

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # [START gae_python37_app]
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__, static_url_path="", static_folder="www")
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 60
-
-@app.route('/')
-def hello():
-    return app.send_static_file("index.html")
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 60
 
 
-if __name__ == '__main__':
+@app.route("/")
+def root():
+    return redirect("/index.html")
+
+
+if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
 # [END gae_python37_app]
