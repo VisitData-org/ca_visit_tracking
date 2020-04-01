3239f6ce0f04a25d95153da7270bf97ce0fd951b
Andrew Janian <andrew@janian.net>
2020-04-01 10:26:09 -0400

    state working with permalinks, no chooser yet

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ def byvenues(venues):
 
 @app.route("/bydate.html")
 def bydate():
-    return render_template("bydate.html", counties="", venues="")
+    return render_template("bydate.html", state="", counties="", venues="")
 
-@app.route("/bydatesel/<counties>/<venues>")
-def bydatesel(counties, venues):
-    return render_template("bydate.html", counties=counties, venues=venues)
+@app.route("/bydatesel/<state>/<counties>/<venues>")
+def bydatesel(state, counties, venues):
+    return render_template("bydate.html", state=state, counties=counties, venues=venues)
 
 @app.route("/report_tree.html")
 def report_tree():
