6cddb7a9971555baf78f13e1b9084ffbe7ec7878
Mark Roth <mark@octagonsoftware.com>
2020-04-01 22:16:25 -0400

    Check git status, record version before deploying

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,36 +5,49 @@ DIR="$(cd "$(dirname $0)"; pwd)"
 
 # Output an error message and exit with an error code
 error() {
-  echo "$0: ERROR: $1" 1>&2
+  echo -e "\x1b[31m$0: ERROR: $1\x1b[0m" 1>&2
   exit 1
 }
 
 
 # Output an info message
 info() {
-  echo "$0: INFO: $1" 1>&2
+  echo -e "\x1b[33m$0: INFO: $1\x1b[0m" 1>&2
 }
 
 
+# Perform git-related validations
 check_git_status() {
   # Check that we're releasing only what was committed
-  git diff-index --quiet HEAD -- || error "Uncommitted changes"
+  git diff-index --quiet HEAD -- || (git status; error "Uncommitted changes")
 
   # Check that we're on the master branch
-  git
+  branch="$(git rev-parse --abbrev-ref HEAD)"
+  [ "${branch}" == "master" ] || error "Not on master branch (on ${branch})"
 }
 
 
+# Put any validations before deploying here
 validate() {
+  info "Validating..."
   check_git_status
 }
 
 
+# Record the version number so we can access it at runtime
+record_version() {
+  git rev-parse --short HEAD > static/siteversion.txt
+}
+
+
+# Deploy
 deploy() {
-  echo "Deploying..."
+  info "Deploying..."
+  gcloud app deploy .
 }
 
 
 cd "${DIR}/.."
 validate
+record_version
 deploy
