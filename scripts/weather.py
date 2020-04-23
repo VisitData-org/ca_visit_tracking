ed4b19c99d6d38b6299c6c88f5c3bc0014344bb8
Angel Garcia <agarciamal1997@gmail.com>
2020-04-23 16:45:53 +0200

    Added functions to download weather data locally or from the cloud

--- scripts/weather.py
+++ scripts/weather.py
@@ -2,7 +2,7 @@ import requests, json
 from datetime import datetime, timedelta
 from deepmerge import always_merger
 from scripts.config import *
-
+from google.cloud import storage
 
 def get_weather_data(query, limit=7):
     weather = {}
@@ -36,7 +36,7 @@ def __update_state_file(state, data):
     with open("{}{}.json".format(DATA_PATH, state), "w+") as f:
         json.dump(data, f)
 
-def get_state_weather(state):
+def get_state_weather_locally(state):
     if state not in STATES:
         return json.dumps(NO_STATE_ERROR_RESPONSE)
     cached_data = __load_state_file(state)
@@ -46,3 +46,9 @@ def get_state_weather(state):
         cached_data[county] = always_merger.merge(county_data, weather_data)
     __update_state_file(state, cached_data)
     return json.dumps(cached_data)
+
+def get_state_weather_cloud(state, bucket_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    file = bucket.get_blob("{}.json".format(state))
+    return json.loads(file.download_as_string()) if file is not None else {}
